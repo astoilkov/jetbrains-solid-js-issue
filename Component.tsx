ea8fd60cdd7d3999a2efa70d93c9cdb2e8a438b4
Antonio Stoilkov <hello@astoilkov.com>
2024-05-17 12:33:20 +0300

    reproduce issue

---
+++ Component.tsx
@@ -0,0 +1,5 @@
+import type { JSXElement } from 'solid-js'
+
+export default function Component(): JSXElement {
+  return <div class="asdf"></div>
+}
---
+++ packages-react/react-effects/Effects.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from "react";
+
+export default function Effects(): ReactElement {
+  return (
+    <div>
+      <h1>Effects</h1>
+    </div>
+  );
+}
